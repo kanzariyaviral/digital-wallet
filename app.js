5112e141c4572b5285c9825dfb0b8a73b68b3208
Viral Kanzariya <viral.kanzariya@gtcsys.com>
2022-08-29 12:53:50 +0530

    test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect(DB)
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers",  
     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
   );
 
