d7dcb76ca7487b482fc7ce3ab66d168892f4280f
Viral Kanzariya <viral.kanzariya@gtcsys.com>
2022-08-29 12:51:21 +0530

    added header allow origin

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ mongoose.connect(DB)
   console.log("connected to database");
 })
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-//   );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
 
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT,POST,DELET,GET");
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,DELET,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "hyyy",
