584ea999800b416cc5b3570ce047de33ea69b906
Viral Kanzariya <viral.kanzariya@gtcsys.com>
2022-09-05 17:27:02 +0530

    fix :get "/by/id"to post request

--- routes/resource.routes.js
+++ routes/resource.routes.js
@@ -5,5 +5,5 @@ const resource=require("../controllers/resource.controller")
 router.post("/create",resource.createResource)
 router.post("/login",resource.login)
 router.get("/",resource.getAllResource)
-router.get("/by/:id",resource.getResourceByID)
+router.post("/by/:id",resource.getResourceByID)
 module.exports = router;
